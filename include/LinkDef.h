616f0f0c1bd14cb1c51e0cac19d6d823ff69a859
Frappa <fpiastra@physik.uzh.ch>
2017-12-04 18:06:40 +0100

    Frappa: With ACLiC had linking problems. Trying with a standalone program.

---
+++ include/LinkDef.h
@@ -0,0 +1,11 @@
+#if defined(__CLING__)
+
+#pragma link off all class;
+#pragma link off all function;
+#pragma link off all global;
+#pragma link off all typedef;
+
+#pragma link C++ defined_in "PaxTree.hh";
+#pragma link C++ defined_in "include/PaxTree.hh";
+
+#endif
